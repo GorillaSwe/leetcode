20a4cd4d6c93388df2ec4eb00e9ce8c2c767ab62
junk237 <reds7.buddy42rich@gmail.com>
2023-05-01 23:58:04 +0900

    modify 9 done

--- Java/9.palindrome-number.java
+++ Java/9.palindrome-number.java
@@ -7,7 +7,16 @@
 // @lc code=start
 class Solution {
     public boolean isPalindrome(int x) {
-        
+        String s = String.valueOf(x);
+        int n = s.length();
+
+        if(x < 0) return false;
+        else {
+            for(int i=0;i<n/2;i++){
+                if(s.charAt(i) != s.charAt(n-i-1)) return false;
+            }
+            return true;
+        }
     }
 }
 // @lc code=end
