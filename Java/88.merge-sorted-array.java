22f31b5c758c5474c349a6317bc729c1d773abfc
junk237 <reds7.buddy42rich@gmail.com>
2023-05-29 23:05:20 +0900

    add 88, 100 done

---
+++ Java/88.merge-sorted-array.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=88 lang=java
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+}
+// @lc code=end
